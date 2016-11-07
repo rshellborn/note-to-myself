9f3f04a4df889770af56dbee52b1f43d058d10b0
rshellborn <rachel@shellborn.com>
2016-11-07 15:50:10 -0800

    setting up registration

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => 'hello@example.com',
-        'name' => 'Example',
+        'address' => 'rshellborndev@gmail.com',
+        'name' => 'Admin',
     ],
 
     /*
